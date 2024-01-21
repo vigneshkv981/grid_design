30db1963e3c22649f4747ac88ecad4a730e83ab4
vigneshtestengineer <vigneshkv6533@gmail.com>
2024-01-21 14:32:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import items_list from './datas';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+            <div class="mainheader">
+            <div class="logo">
+                EDYODA
+            <p>Stories</p>
+            </div>
+ 
+        <div class="Explorecategory">
+            Explore Categories
+            <i class="bi-chevron-down"></i>
+        </div>
+        </div>
+        </nav>
+
       </header>
-    </div>
+
+       <div class="mainbody" id="maincard">
+  
+          <h3 class="trendingheader">Trending Posts</h3>
+
+          {/* <!-- -------Categories----------- --> */}
+<div class="Categoriescontainer">
+  <div class="container header">
+      <div class="filter">
+          <i class="bi-funnel-fill"></i>
+          <p class="filterpara">Filter by Category</p>
+      </div>
+  </div>
+  <div class="Categoriesbuttons">
+      <button class="btn active">All</button>
+      <button class="btn">Artificial Intelligence</button>
+      <button class="btn">Cloud Computing</button>
+      <button class="btn">DevOps</button>
+      <button class="btn">Programming Languages</button>
+      <button class="btn">Mobile Application Development</button>
+      <button class="btn">Technology and Tools</button>
+      <button class="btn">Get a Job in a Tech Company</button>
+      <button class="btn">Others</button>
+  </div>
+</div>
+          
+
+
+
+
+    <div class="allitems" id="alliteams">
+      {items_list.map((story_item) => (
+          <>
+          <div class="itemsections">
+         
+
+        <div class="itemssec">
+          <div class="imagecontainer">
+          <img className='images' src={story_item.story_img}></img>
+          </div>
+
+       <div class="containerbody">
+          <h3 class="containerbodyheader">{story_item.story_header}</h3>
+          <p class="containerlogo">EdYoda
+              <span class="containerdate">
+                  |
+                  {story_item.date}
+              </span>
+          </p>
+
+          <div class="containerpara">
+                <p>{story_item.para_content}</p>
+          </div>
+        </div>
+        </div>
+    
+        </div>
+                 
+          </>
+
+
+))}
+    
+
+
+
+</div>
+
+         </div>
+         </div>   
+
+
   );
 }
 
---
+++ src/datas.js
@@ -0,0 +1,93 @@
+const items_list = [
+
+    {
+   
+     id: "1",
+   
+     para_content: "We have had an insight into Artificial Intelligence. Well, one of its subfields is machine learning and this introduction will help you get a fair idea of what this machine learning....",
+
+     date: '15 Oct 2023',
+   
+     story_header: "Introduction To A Machine Learning Online Course",
+   
+     story_img: "https://edyoda.s3.amazonaws.com/media/blog-images/learn_machine_learning_courses_online.png",
+   
+    },
+   
+    {
+   
+        id: "2",
+      
+        para_content: "In today’s dynamic world, developing a rapid application with a smooth user interface that can adapt to specific features is the main business requirement. Many....",
+   
+        date: '20 Oct 2023',
+      
+        story_header: "LAMP Stack Vs MEAN stack: Choosing the right",
+      
+        story_img: "https://edyoda.s3.amazonaws.com/media/blog-images/6.png",
+      
+       },
+   
+       {
+   
+        id: "3",
+      
+        para_content: "Cloud computing has been rapidly gaining pace in the world of information technology. It has been observed that over 90% of global enterprises are using cloud....",
+   
+        date: '25 Oct 2023',
+      
+        story_header: "Impact of Cloud Computing in various",
+      
+        story_img: "https://edyoda.s3.amazonaws.com/media/blog-images/4.png",
+      
+       },
+   
+       {
+   
+        id: "4",
+      
+        para_content: "Machine Learning is a subset of artificial intelligence which is an important part of computer science. The revolution of Big Data promises to transform the way we....",
+   
+        date: '1 Nov 2023',
+      
+        story_header: "Challenges of Machine Learning in Big Data",
+      
+        story_img: "https://edyoda.s3.amazonaws.com/media/blog-images/3.png",
+      
+       },
+   
+       {
+   
+        id: "5",
+      
+        para_content: "Cybersecurity plays an integral role in the process of good business models. But even cybersecurity programs built with good intentions can fall short in....",
+   
+        date: '10 Nov 2023',
+      
+        story_header: "Breaking the Myths around Cybersecurity",
+      
+        story_img: "https://edyoda.s3.amazonaws.com/media/blog-images/5.png",
+      
+       },
+
+       {
+   
+        id: "6",
+      
+        para_content: "A business must always introspect the areas where they lack in order to bring about a positive change in the work environment.....",
+   
+        date: '23 Nov 2023',
+      
+        story_header: "Importance of Data Backup and Recovery in",
+      
+        story_img: "https://edyoda.s3.amazonaws.com/media/blog-images/7.png",
+      
+       },
+
+
+   
+   ];
+
+   export default items_list;
+   
+   
