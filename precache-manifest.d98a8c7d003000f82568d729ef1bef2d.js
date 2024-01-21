92cbbf6a131e8e30b177d983a629554dc97340e2
vigneshkv981 <st.kvvignesh@gmail.com>
2024-01-21 18:55:03 +0530

    Updates

---
+++ precache-manifest.d98a8c7d003000f82568d729ef1bef2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aef01dc45e6726fb5b0b87cd4a7b917",
+    "url": "/grid_design/index.html"
+  },
+  {
+    "revision": "d1ddadd8b40630591ec1",
+    "url": "/grid_design/static/css/main.d4496049.chunk.css"
+  },
+  {
+    "revision": "c31bfd1f1cfe6edb2b02",
+    "url": "/grid_design/static/js/2.65fcd7b0.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/grid_design/static/js/2.65fcd7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9736bbdff6f115f0596b",
+    "url": "/grid_design/static/js/3.139da1ac.chunk.js"
+  },
+  {
+    "revision": "d1ddadd8b40630591ec1",
+    "url": "/grid_design/static/js/main.36b26afb.chunk.js"
+  },
+  {
+    "revision": "6f00e0901acc4c18925b",
+    "url": "/grid_design/static/js/runtime-main.d08bed4a.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/grid_design/static/media/logo.06e73328.svg"
+  }
+]);
